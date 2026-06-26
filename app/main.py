8dff4b9bfc683d8632a1550dd64b0444a63eb7a6
Naitik <sethnaitikgg77@gmail.com>
2026-06-26 23:17:39 +0530

    Add Docker and Docker Compose support

--- app/main.py
+++ app/main.py
@@ -5,7 +5,6 @@ from app.routes import auth, hackathon, leaderboard,teams,submisions,votes
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router)
 app.include_router(hackathon.router)
